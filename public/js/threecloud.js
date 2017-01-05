c4eb475fca448a9d1b2b5f442ec3dc292b8d493e
Bill Roy <bill@bitlash.net>
2017-01-05 09:47:02 -0700

    index3 working

--- public/js/threecloud.js
+++ public/js/threecloud.js
@@ -10,8 +10,8 @@ function init(chartData) {
     //container = document.createElement( 'div' );
     //document.body.appendChild( container );
     container = document.getElementById('chart');
-    camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 2, 5000 );
-    camera.position.z = 1000;
+    camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 2, 5000 );
+    camera.position.z = 2000;
     scene = new THREE.Scene();
     //scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
     geometry = new THREE.Geometry();
@@ -30,6 +30,14 @@ function init(chartData) {
         vertex.y = point.y;
         vertex.z = point.z;
         geometry.vertices.push(vertex);
+
+        var spritey = makeTextSprite(point.name, {
+            fontsize: 24,
+            borderColor: point.color,           // {r:255, g:0, b:0, a:1.0},
+            backgroundColor: {r:100, g:100, b:100, a:0.8}
+        });
+        spritey.position.set(point.x, point.y, point.z);
+        scene.add( spritey );
     });
 
     material = new THREE.PointsMaterial( {
@@ -37,11 +45,12 @@ function init(chartData) {
         size: 35,
         sizeAttenuation: true,
         alphaTest: 0.5,
-        transparent: true
+        transparent: true,
     } );
     material.color.setHSL( 1.0, 0.3, 0.7 );
     particles = new THREE.Points( geometry, material );
     scene.add( particles );
+
     //
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -70,6 +79,95 @@ function init(chartData) {
 	controls.keys = [ 65, 83, 68 ];
     controls.addEventListener( 'change', render );
 }
+
+function makeTextSprite( message, parameters ) {
+	if ( parameters === undefined ) parameters = {};
+
+	var fontface = parameters.hasOwnProperty("fontface") ?
+		parameters["fontface"] : "Arial";
+
+	var fontsize = parameters.hasOwnProperty("fontsize") ?
+		parameters["fontsize"] : 18;
+
+	var borderThickness = parameters.hasOwnProperty("borderThickness") ?
+		parameters["borderThickness"] : 4;
+
+	var borderColor = parameters.hasOwnProperty("borderColor") ?
+		parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+
+	var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+		parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+
+	//var spriteAlignment = THREE.SpriteAlignment.topLeft;
+
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	context.font = "Bold " + fontsize + "px " + fontface;
+
+	// get size data (height depends only on font size)
+	var metrics = context.measureText( message );
+	var textWidth = metrics.width;
+
+	// background color
+    if (typeof(backgroundColor) == 'object') {
+    	context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+    								  + backgroundColor.b + "," + backgroundColor.a + ")";
+    }
+    else context.fillStyle = backgroundColor;
+
+	// border color
+    if (typeof(borderColor) == 'object') {
+    	context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+    								  + borderColor.b + "," + borderColor.a + ")";
+    }
+    else context.strokeStyle = borderColor;
+
+	context.lineWidth = borderThickness;
+	roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+	// 1.4 is extra height factor for text below baseline: g,j,p,q.
+
+	// text color
+	//context.fillStyle = "rgba(0, 0, 0, 1.0)";
+    context.fillStyle = "rgba(255, 255, 255, 0.8)";
+
+	context.fillText( message, borderThickness, fontsize + borderThickness);
+
+	// canvas contents will be used for a texture
+	var texture = new THREE.Texture(canvas)
+	texture.needsUpdate = true;
+
+	var spriteMaterial = new THREE.SpriteMaterial({
+        map: texture,
+        //useScreenCoordinates: false,
+        //alignment: spriteAlignment
+        // TODO: fog: true
+    });
+	var sprite = new THREE.Sprite( spriteMaterial );
+	sprite.scale.set(100,50,1.0);
+	return sprite;
+}
+
+// function for drawing rounded rectangles
+function roundRect(ctx, x, y, w, h, r) {
+    ctx.beginPath();
+    ctx.moveTo(x+r, y);
+    ctx.lineTo(x+w-r, y);
+    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+    ctx.lineTo(x+w, y+h-r);
+    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+    ctx.lineTo(x+r, y+h);
+    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+    ctx.lineTo(x, y+r);
+    ctx.quadraticCurveTo(x, y, x+r, y);
+    ctx.closePath();
+    ctx.fill();
+	ctx.stroke();
+}
+
+
+
+
+
 function onWindowResize() {
     windowHalfX = window.innerWidth / 2;
     windowHalfY = window.innerHeight / 2;
--- testbig.py
+++ testbig.py
@@ -1,4 +1,5 @@
 import json
+import math
 import random
 import requests
 
